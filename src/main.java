6846114520ac2e3d769a434c0fa6409fa2b2c973
Doudrick <tyler.doudrick@gmail.com>
2014-01-29 09:51:46 -0500

    test

--- src/main.java
+++ src/main.java
@@ -1,6 +1,18 @@
-/**
- * Created by admin on 1/29/14.
- */
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
 public class main {
+    public Main(){
+        try {
+            Display.setDisplayMode(new DisplayMode(800, 600));
+        } catch (LWJGLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args){
+        new main();
+    }
 }
 
