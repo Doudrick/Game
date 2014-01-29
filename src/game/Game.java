eed69cf1b39245fa3a8d290e1b84dea41f0a44af
Doudrick <tyler.doudrick@gmail.com>
2014-01-29 09:55:15 -0500

    Test project2

--- src/game/Game.java
+++ src/game/Game.java
@@ -13,7 +13,6 @@ import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 
-
 /**
  * @author jediTofu
  * @see <a href="http://lwjgl.org/">LWJGL Home Page</a>
